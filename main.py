12bff0ba966b8b494709d6ac3425a655b174be4b
isr-martinez1224 <161985666+isr-martinez1224@users.noreply.github.com>
2026-01-23 13:06:03 -0600

    Updates to main.py - Basic UI layout started

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
-print("Hello")
+from tkinter import *
+
+# --------------------- UI SETUP -------------------------#
+
+window = Tk()
+window.title("Python Fitness Tracker")
+window.minsize(width=600, height=450)
+window.config(padx=20, pady=20)
+
+# canvas = Canvas(width=600, height=450, highlightthickness=0)
+# canvas_text = canvas.create_text(300, 75, text="Python Fitness App", fill="blue", font=("Impact", 25, "bold"))
+# canvas.grid()
+
+title_label = Label(text="Python Fitness App", fg= "blue", font=("Impact", 35))
+title_label.grid(column=1, row=0)
+
+def log_off():
+    pass
+
+exit_button = Button(text="Log Off", command=log_off, highlightthickness=0)
+exit_button.grid(column=1, row=3)
+
+
+
+
+window.mainloop()
