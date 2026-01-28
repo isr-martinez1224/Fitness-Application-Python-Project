9c8049758bb2d935ffb4a545ac7a693787e9e772
isr-martinez1224 <161985666+isr-martinez1224@users.noreply.github.com>
2026-01-28 12:21:08 -0600

    Updates to main.py - More UI updates, main structure implemented

--- main.py
+++ main.py
@@ -4,22 +4,31 @@ from tkinter import *
 
 window = Tk()
 window.title("Python Fitness Tracker")
-window.minsize(width=600, height=450)
 window.config(padx=20, pady=20)
 
-# canvas = Canvas(width=600, height=450, highlightthickness=0)
-# canvas_text = canvas.create_text(300, 75, text="Python Fitness App", fill="blue", font=("Impact", 25, "bold"))
-# canvas.grid()
+# Canvas/Image setup
+canvas = Canvas(width=260, height=260, highlightthickness=0)
+logo_img = PhotoImage(file="workout-icon.png")
+canvas.create_image(125, 125, image=logo_img)
+canvas.grid(column=1, row=2)
 
-title_label = Label(text="Python Fitness App", fg= "blue", font=("Impact", 35))
-title_label.grid(column=1, row=0)
+title_label = Label(text="Python Fitness App", fg= "#44cfeb", font=("Impact", 35))
+title_label.grid(column=1, row=0, sticky="EW")
 
-def log_off():
+
+#View Measurements
+def view_measurements():
     pass
 
-exit_button = Button(text="Log Off", command=log_off, highlightthickness=0)
-exit_button.grid(column=1, row=3)
+view_measurements_button = Button(text="View Measurements", command=view_measurements, highlightthickness=0, bg="#44eb5a")
+view_measurements_button.grid(column=1, row=3, sticky="EW")
 
+#Log Off Button - Closes the app
+def log_off():
+    window.destroy()
+
+exit_button = Button(text="Log Off", command=log_off, highlightthickness=0, bg="red", fg="white")
+exit_button.grid(column=1, row=4)
 
 
 
